9e205c1953a4ca730ccaa590e253d0cd7c817ea0
Luis Eduardo Martins <88583199+LuisEduMartins@users.noreply.github.com>
2023-11-27 21:10:48 -0300

    Add files via upload

---
+++ Prog Funcional Verificador de cartão.hs
@@ -0,0 +1,55 @@
+
+addSum :: String -> String
+addSum input =
+  let digitos = map (\c -> read [c] :: Int) input -- Converte cada dígito para um inteiro
+      sumDigits = sum digitos -- Soma os dígitos
+      verificadores = show sumDigits -- Verificadores recebe a soma dos números transformada em String
+  in input ++ verificadores -- Concatena o 8 primeiros dígitos com os verificadores
+
+validaCartao :: String -> String
+validaCartao cartao
+    |length (cartao) /=10 = "Cartão Invalido"
+    |somaOitoNum(take 8 cartao) == ultimos(drop 8 cartao) = "Cartão valido"
+    |otherwise = "Cartão invalido"
+
+
+ultimos :: String -> Int
+ultimos num = read(reverse(take 2 (reverse num))) :: Int
+
+somaOitoNum :: String -> Int
+somaOitoNum num 
+    |((length num <=8) && (length num>0)) = (read(take 1 num) :: Int) + somaOitoNum (drop 1 num)
+    |otherwise = 0
+
+Questão 3 (1).txt
+﻿import Data.Char (toLower)
+
+
+eVogal :: Char -> Bool
+eVogal c = toLower c `elem` "aeiou"
+
+
+eConsoante :: Char -> Bool
+eConsoante c = toLower c `elem` "bcdfghjklmnpqrstvwxyz"
+
+
+contaVogais :: String -> Int
+contaVogais = length . filter eVogal
+
+
+contaConsoante :: String -> Int
+contaConsoante = length . filter eConsoante
+
+
+validaSenha :: String -> Bool
+validaSenha senha =
+  let len = length senha
+      vogais = contaVogais senha
+      consoantes = contaConsoante senha
+  in len >= 4 && len <= 8 && vogais >= 2 && consoantes >= 2
+
+
+
+filterBadPassword :: [String] -> [String]
+filterBadPassword [] = []
+filterBadPassword lista = filter validaSenha lista
